884d97b189a9fc9279ae6bee1c3562558c823862
kalash0804 <167464704+kalash0804@users.noreply.github.com>
2024-08-26 18:27:43 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
                 <Link to="/" >   <img className=" w-25"  src="/imgs/logo.png" alt="logo here"/> </Link>
             </div>
             <div className="col-sm-5">
-                <b>   <i> Creating Happy Home Owners Everyday </i> </b>
+                <b>   <i> Jaldi aao jldi pao </i> </b>
             </div>
             <div className="col-sm-3">
             {
